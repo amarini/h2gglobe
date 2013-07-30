6252d427e0d116d9693b3683c3d654f53dfc2394
Andrea Marini <andrea.marini@cern.ch>
2013-07-30 14:15:04 +0200

    mod dir in submit.sh

--- AnalysisScripts/python/submit.sh
+++ AnalysisScripts/python/submit.sh
@@ -2,7 +2,8 @@
 
 FILE="scale"
 DIR="/eos/cms/store/group/phys_higgs/cmshgg/processed/V15_00_08/mc/Summer12_DR53X-PU_RD1_START53_V7N/DiPhotonJetsBox_M60_8TeV-sherpa_Summer12_DR53X-PU_RD1_START53_V7N-v1/"
-OUT="$PWD/output"
+DIR="/eos/cms/store/group/phys_higgs/cmshgg/processed/V15_00_05/mc/Summer12_DR53X-PU_S10_START53_V7C/DiPhotonJetsBox_M60_8TeV-sherpa_Summer12_DR53X-PU_S10_START53_V7C-v1_v3"
+OUT="$PWD/outputV150005"
 QUEUE=8nh
 NJOBS=100
 
